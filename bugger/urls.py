d4796da3f9a9a3acd38be48daf17af4ada89e02a
daniel <daniel@fry.(none)>
2014-02-22 19:36:13 +1100

    Fixed URL issue

--- bugger/urls.py
+++ bugger/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from bugger import views
 
 urlpatterns = patterns('',
+        url(r'^create', 'bugger.views.create_bug'),
         url(r'^', 'bugger.views.listBugs'),        
-	url(r'^create', 'bugger.views.create_bug'),
 )
 
