fdf6e216bae9897a7e1f7ff0c595c12f06ba68f6
markt <mtevans@gmail.com>
2014-03-02 13:04:23 +1100

    fixed the messaging issues. Appears to be working fine.

--- teams/views.py
+++ teams/views.py
@@ -39,36 +39,29 @@ def group_detail(request, group_id):
     User = request.user
     memlist = listMembershipList(User)
     leap_list = Leap.objects.filter(group=group).order_by('-created_at')
-    form2 = leap_form()
+    form2 = leap_form(prefix='postMessage')
     if group.group_name not in memlist:
         return HttpResponse("You are not authorized to view this group.")
     else:
         auth = isOwner(User,group)
-        print auth
         if auth:
-            form = models.add_member_form()
+            form = models.add_member_form(prefix='addMember')
             if request.method == 'POST':
                 form = models.add_member_form(request.POST, prefix='addMember' )
                 if form.is_valid():
-                    new_member=form.save(group)
-                    try:
-                        new_leap = Leap(message='[SYSTEM GENERATED MESSAGE] Adding: ' + new_member.user.username  + ' as '+form.cleaned_data['membership_type']+'.',author=User,group=group)
-                        new_leap.save()
-                        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-                    except AttributeError:
-                        return HttpResponse("User does not exist !, Please click the back button on your browser and try again.")
+                    new_member = form.save(group)
+                    print new_member
                 else:	
                     print "tstt"
                     form2 = leap_form(request.POST, prefix ='postMessage')
                     form = models.add_member_form(prefix='addMember')
-                    return render(request, 'teams/group_detail.html', {'group': group,'form':form, 'form2':form2}) 
-                if form2.is_valid():
-                    print "tsttt2"
-                    form2.save(group,User)
-                    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-                else:
-                    form2=leap_form(prefix='postMessage')
-                return render(request, 'teams/group_detail.html', {'group': group,'leap_list':leap_list,'form':form,'form2':form2})           
+                    if form2.is_valid():
+                        print "tsttt2"
+                        form2.save(group,User)
+                        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+                    else:
+                        form2=leap_form(prefix='postMessage')
+            return render(request, 'teams/group_detail.html', {'group': group,'leap_list':leap_list,'form':form,'form2':form2})           
         else:
             print "NOT Owner"
             form2 = leap_form()
